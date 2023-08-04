23a111d6417ae649c1a50029a257ad071b04e7c1
Ata81051 <141276642+Ata81051@users.noreply.github.com>
2023-08-04 22:44:13 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://www.cypernews.info/graphql"
+	const endpoint = "https://bestfreeseotools.pro/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://www.cypernews.info/` + encodeURI(path as string)
+					`https://bestfreeseotools.pro/` + encodeURI(path as string)
 				}`,
 			},
 		};
